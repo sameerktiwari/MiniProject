e99f791bed243e27c3bc4faeaf0e3fbf71369fc4
iit2013159 <iit2013159@iiita.ac.in>
2017-10-31 17:39:52 +0530

    Interface of participant and committe added

--- MINIProject/src/com/university/service/IComittee.java
+++ MINIProject/src/com/university/service/IComittee.java
@@ -1,5 +1,14 @@
 package com.university.service;
 
+import java.util.Map;
+
+import com.university.entities.Application;
+import com.university.entities.ProgramScheduled;
 public interface IComittee {
+	/*
+	 * He/She must be able to view all the applicant data based on Scheduled
+	 * program id
+	 */
+	Map<Integer, Application> viewApplication(ProgramScheduled programScheduled);
 
 }
---
+++ MINIProject/src/com/university/service/IParticipant.java
@@ -0,0 +1,31 @@
+package com.university.service;
+
+import java.awt.List;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import com.university.entities.ProgramScheduled;
+
+public interface IParticipant {
+	/*
+	 * This option provides the applicant with information related to programs
+	 * scheduled by the university, this screen has option to Apply for Program.
+	 */
+	ArrayList<ProgramScheduled> viewScheduledProgram();
+
+	/*
+	 * Allows an applicant to fill in and submit application form for a
+	 * scheduled program, an applicant ID is auto generated at DB level
+	 */
+	void apply(ProgramScheduled programScheduled);
+	
+	/*
+	 * An applicant should be able to view the status of application by entering the Applicantion_id
+	 */
+	void viewApplication(String application_Id);
+	
+	/*
+	 * View information of Programs Scheduled to Commence in a given time period
+	 */
+	void getAllProgramInPeriod(LocalDate start, LocalDate end);
+}
